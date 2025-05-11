514e8c3b9190b5b3ffb97b13327377e113030979
victoria-menezes <victoria.c.menezes@gmail.com>
2025-05-11 20:36:07 -0300

    Comecar com line graphs

---
+++ Challenge/my_functions.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+from cycler import cycler
+import numpy as np
+import seaborn as sb
+from datetime import date
+import matplotlib.ticker as ticker
+import matplotlib
+
+def rotate_label(rot : float = 45, ha : str ='right'):
+    plt.xticks(rotation=rot, ha=ha)
+
+def add_grid(ax):
+    ax.set_axisbelow(True)
+    plt.grid(zorder=-1, linestyle='--')
+
+def set_y_lim(df : pd.DataFrame, column : str, percentage : float = 0.2):
+    max = df.max()[column]
+    plt.ylim(0, max*(1 + percentage))
+
+def set_labels(x : str = '', y : str = ''):
+    plt.xlabel(x)
+    plt.ylabel(y)
+
+def ticks_on_years(ax, axis='x'):
+    if axis == 'x':
+        ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+    else:
+        ax.yaxis.set_major_locator(matplotlib.dates.YearLocator())
+    return ax
+
+def ticks_per_interval(ax, base, offset, axis='x',):
+    if axis == 'x':
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(base, offset=offset))
+    else:
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(base, offset=offset))
